69e159d2e2f872bccf906f1ff5d98d67e89dec6b
8amjp <wakeup@8am.jp>
2018-04-15 18:22:56 +0900

    version 0.1.5

--- src/extension.ts
+++ src/extension.ts
@@ -1,19 +1,8 @@
 'use strict';
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
 import { window, commands, ExtensionContext, TextEditor, Position } from 'vscode';
 
-// this method is called when your extension is activated
-// your extension is activated the very first time the command is executed
 export function activate(context: ExtensionContext) {
 
-    // Use the console to output diagnostic information (console.log) and errors (console.error)
-    // This line of code will only be executed once when your extension is activated
-    console.log('Congratulations, your extension "8novels-formatter" is now active!');
-
-    // The command has been defined in the package.json file
-    // Now provide the implementation of the command with  registerCommand
-    // The commandId parameter must match the command field in package.json
     commands.registerCommand('extension.formatNovel', () => {
         if (window.activeTextEditor) {
             let editor = window.activeTextEditor;
@@ -64,6 +53,5 @@ function indent(editor: TextEditor): Promise<boolean> {
     });
 }
 
-// this method is called when your extension is deactivated
 export function deactivate() {
 }
